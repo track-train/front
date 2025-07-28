db553246a56d4b0df029cd3afcb6d232ae350c9e
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-28 16:36:11 +0200

    create the snackbar component

---
+++ src/components/AppSnackbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-snackbar
+    v-model="snackbarStore.show"
+    :timeout="snackbarStore.timeout"
+    :color="snackbarStore.color"
+    location="top right"
+    multi-line
+  >
+    {{ snackbarStore.message }}
+    <template #actions>
+      <v-btn icon @click="snackbarStore.close()">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script setup>
+import { useSnackbarStore } from '@/stores/snackbar'
+const snackbarStore = useSnackbarStore()
+</script>
