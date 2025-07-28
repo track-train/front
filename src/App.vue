e0854f79727816d50aa2994743efeb899c71529a
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-28 16:36:37 +0200

    use snackbar in app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <VContainer fluid class="pa-14" style="height: 100vh">
     <RouterView />
+    <AppSnackbar />
   </VContainer>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import AppSnackbar from './components/AppSnackbar.vue'
 </script>
 
 <style lang="scss" scoped></style>
