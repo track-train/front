91ed7e66a4f0ce9f063e09ccdba4676823f9d2b5
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-30 12:57:43 +0200

    create dynamic homepage when connecte or not connected

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import AppSnackbar from './components/AppSnackbar.vue'
+import { useAuthStore } from './stores/auth'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  await authStore.initialize()
+})
 </script>
 
 <style lang="scss" scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,32 +1,49 @@
 <template>
   <div class="home-page">
-    <h1>Coach Profiles</h1>
-    <div class="coach-list">
-      <CoachCard
-        v-for="coach in coaches"
-        :key="coach.id"
-        :name="coach.name"
-        :description="coach.description"
-      />
-    </div>
+    <template v-if="auth.user">
+      <h1>Bienvenue {{ auth.user.name }}</h1>
+      <div class="user-section">
+        <!-- Card coach actuel -->
+        <UserCoachCard :coach="auth.user.coach" />
+        <!-- Cards personnalisées -->
+        <UserTrainingsCard :trainings="auth.user.trainings" />
+        <UserMealsCard :meals="auth.user.meals" />
+        <UserGraphs :data="auth.user.stats" />
+      </div>
+    </template>
+    <template v-else>
+      <h1>Coach Profiles</h1>
+      <p>
+        Bienvenue sur TrackTrain! Trouvez votre coach et suivez vos progrès. Inscrivez-vous pour une
+        expérience personnalisée.
+      </p>
+      <div class="coach-list">
+        <CoachCard
+          v-for="coach in coaches"
+          :key="coach.id"
+          :name="coach.name"
+          :description="coach.description"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
 import api from '@/plugins/axios'
 import CoachCard from '@/components/CoachCard.vue'
 
+// Store d'authentification
+const auth = useAuthStore()
+console.log('Auth Store:', auth)
 const coaches = ref([])
 
 onMounted(async () => {
-  try {
-    const response = await api.get('/profiles/coachs')
-    console.log('Fetched coach profiles:', response.data)
-    coaches.value = response.data
-  } catch (error) {
-    console.error('Failed to fetch coach profiles:', error)
-  }
+  // Charger la liste des coachs si besoin
+  const response = await api.get('/profiles/coachs')
+  coaches.value = response.data
 })
 </script>
 
