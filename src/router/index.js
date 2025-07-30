078f1e760f6dd0db0159b968ce305e9ca3a4c9b6
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-30 12:07:12 +0200

    fix router for redirection when 401

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { useAuthStore } from '@/stores/auth'
 import Login from '@/pages/LoginPage.vue'
 import Dashboard from '@/pages/DashboardPage.vue'
 import Register from '@/pages/RegisterPage.vue'
+import HomePage from '@/pages/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Login },
+    { path: '/', component: HomePage },
+    { path: '/login', component: Login },
     { path: '/register', component: Register },
     { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
   ],
@@ -16,7 +18,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
   if (to.meta.requiresAuth && !authStore.token) {
-    next('/')
+    next('/login')
   } else {
     next()
   }
